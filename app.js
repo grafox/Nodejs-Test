103c126ac2296d77e8595e6cc671e82a545ae64e
Gassan <g_fox_fts@yahoo.com>
2020-05-23 04:16:30 +0200

    ////

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-/* const logger = require('./logger');
-
+ const logger = require('./logger');
 const path = require('path');
 
 const pathObj = path.parse(__filename);
 console.log(pathObj);
- */
+////////////////
+
  const os = require('os');
 
  const totalMemory = os.totalmem();
@@ -12,5 +12,25 @@ console.log(pathObj);
 
  console.log(`Total Memory: ${totalMemory}`);
  console.log(`Free Memory: ${freeMemory}`);
+/////////////
+ const fs = require('fs');
 
- 
+ files = fs.readdirSync('./', function(err,files){
+     if (err) console.log('Eror',err);
+     else console.log('resulat',files) 
+ });
+ console.log(files);
+/////////////
+
+ const Logger = require('./logger');
+ const logger = new Logger();
+//Register 
+logger.on('messageLogged',(arg)=>{
+    console.log('Listner called',arg);
+})
+
+logger.log('message');
+
+ ///////HTTP
+
+
--- logger.js
+++ logger.js
@@ -1,14 +1,17 @@
 console.log(__filename);
 console.log(__dirname);
 
+const EventEmitter =  require('events');
 
-const url = 'http://mylogger.io/log'
+const url = 'http://mylogger.io/log';
 
-function log(message) {
-    console.log(message);
-    
+class Logger extends EventEmitter{
+    log(message) {
+        console.log(message);
+
+        //Raise an event
+        this.emit('massagelogged',{id: 1,url:'http://'});
+    }
 }
 
-module.export = log;
-
-
+module.exports = Logger;
